71f295b83297eb0afcb998f8cddcc5ddf59b3b7e
Alison Lee <studycatcat@yahoo.com>
2022-03-07 11:52:55 -0800

    beesbeesbees es6 complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,20 @@
-class Bee {
+class Bee extends Grub{
   // TODO..
+  constructor(){
+    // super();  //wat? calls a super without para and that's it ?  Man
+    //ok so we call super() inside of the constructor, to inherite from Grub
+    super()
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing'
+  }
+
+  // constructor(food){
+  //   // if we do take in food as para and call super on food, it will only inherite food property?
+  //   super(food)
+  //   this.age = 5;
+  //   this.color = 'yellow';
+  //   this.job = 'Keep on growing'
+  // }
+
 };
